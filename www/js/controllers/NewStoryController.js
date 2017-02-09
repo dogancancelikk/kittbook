5c3db1f8506380b59b2383f1261325ebba7b46fc
dogancancelikk <dogancancelikk@gmail.com>
2017-02-10 00:03:25 +0300

    imagepicker

--- www/js/controllers/NewStoryController.js
+++ www/js/controllers/NewStoryController.js
@@ -3,7 +3,7 @@
 angular
 .module("starter")
 
-.controller('NewStoryController',function($scope,$ionicHistory,$rootScope,StoryService,ionicMaterialInk,$state,$ionicSlideBoxDelegate,$timeout,$ionicLoading,$mdBottomSheet,$cordovaFile,CategoryService){
+.controller('NewStoryController',function($scope,$ionicPlatform,$ionicHistory,$cordovaImagePicker,$rootScope,StoryService,ionicMaterialInk,$state,$ionicSlideBoxDelegate,$timeout,$ionicLoading,$mdBottomSheet,$cordovaFile,CategoryService){
 	$scope.$parent.clearFabs();
 	$timeout(function() {
 			 $scope.$parent.hideHeader();
@@ -46,6 +46,28 @@ angular
 // 				height:2800,
 // 				fullscreen_new_window : true
 // 		};
+
+$ionicPlatform.ready(function(){
+	$scope.addImageToStory = function(){
+		// Image picker will load images according to these settings
+			var options = {
+			maximumImagesCount: 1, // Max number of selected images, I'm using only one for this example
+			width: 800,
+			height: 800,
+			quality: 80            // Higher is better
+			};
+
+			$cordovaImagePicker.getPictures(options).then(function (results) {
+							// Loop through acquired images
+			for (var i = 0; i < results.length; i++) {
+					console.log('Image URI: ' + results[i]);   // Print image URI
+			}
+			}, function(error) {
+			console.log('Error: ' + JSON.stringify(error));    // In case of error
+			});
+	}
+});
+
 
 
   $scope.createStory=function(story){
